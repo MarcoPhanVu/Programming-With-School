75d6c2c33d266f25d9513e0f4c28d92aee550371
KiteryuKatori <88922348+KiteryuKatori@users.noreply.github.com>
2022-05-27 12:16:36 +0700

    update more files

--- Algorithms/findLength.cpp
+++ Algorithms/findLength.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 
-// int findLength(int arr[]) {
-//     return sizeof(arr) / sizeof(int);
-// }
 
 int main() {
     int arr1[10];
     int arr2[10] = {22, 55, 33, 11, 66, 77, 24, 99, 44, 88};
+    int arr3[8] = {22, 11, 66, 77, 24, 99, 44, 88};
+    int arr4[5] = {22, 11, 77, 24, 99};
     std::cout << "Len of arr1 = " << sizeof(arr1) / sizeof(int) << "\n"; // = 10
     std::cout << "Len of arr2 = " << sizeof(arr2) / sizeof(int) << "\n"; // = 10
+    std::cout << "Len of arr3 = " << sizeof(arr3) / sizeof(int) << "\n"; // = 8
+    std::cout << "Len of arr4 = " << sizeof(arr4) / sizeof(int) << "\n\n"; // = 5
     return 0;
 }
